b0a21a924b6a8237cb828689c230872dadc2130c
mehateam <mehateam06@gmail.com>
2026-05-05 01:39:11 +0900

    Add script.js, .gitignore, package-lock.json; improve static file server

--- server.js
+++ server.js
@@ -4,10 +4,30 @@ const path = require('path');
 
 const PORT = process.env.PORT || 3000;
 
+const MIME = {
+  '.html': 'text/html; charset=utf-8',
+  '.js':   'application/javascript',
+  '.css':  'text/css',
+  '.png':  'image/png',
+  '.jpg':  'image/jpeg',
+  '.svg':  'image/svg+xml',
+  '.ico':  'image/x-icon',
+};
+
 http.createServer((req, res) => {
-  fs.readFile(path.join(__dirname, 'index.html'), (err, data) => {
-    if (err) { res.writeHead(500); res.end('Error'); return; }
-    res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' });
+  let filePath = req.url === '/' ? '/index.html' : req.url;
+  filePath = path.join(__dirname, filePath);
+  const ext = path.extname(filePath);
+
+  fs.readFile(filePath, (err, data) => {
+    if (err) {
+      fs.readFile(path.join(__dirname, 'index.html'), (e, d) => {
+        res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' });
+        res.end(d);
+      });
+      return;
+    }
+    res.writeHead(200, { 'Content-Type': MIME[ext] || 'text/plain' });
     res.end(data);
   });
 }).listen(PORT, () => console.log(`AurenCars running on port ${PORT}`));
